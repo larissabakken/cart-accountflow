25d799e3e62cff591bdcdd448f5caad3090f384d
Larissa Ramos <devlarissasr@gmail.com>
2023-03-17 11:50:17 -0300

    feat: utils

---
+++ src/utils.js
@@ -0,0 +1,20 @@
+export const calculateTotalPrice = (cartItems) => {
+    const totalPrice = cartItems.reduce((total, item) => {
+      return total + item.price * item.quantity;
+    }, 0);
+  
+    return totalPrice.toFixed(2);
+  };
+
+  export function addToCart(cartItems, product) {
+    const existingItem = cartItems.find((item) => item.id === product.id);
+  
+    if (existingItem) {
+      return cartItems.map((item) =>
+        item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+      );
+    }
+  
+    return [...cartItems, { ...product, quantity: 1 }];
+  }
+  
