85826f9b6ee80538c00fd03e96434c8651ba28b2
Larissa Ramos <devlarissasr@gmail.com>
2023-03-17 11:51:08 -0300

    fix: changing props ono list products

--- src/ListProducts.jsx
+++ src/ListProducts.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Product from "./Product";
 
-export default function Main(props) {
-  const products = props.data;
+export default function ListProducts(props) {
+  const {products, addToCart} = props;
 
   return (
     <main className="block col-2">
       <h2>Products</h2>
       <div className="row">
       {products?.map((item) => {
-        return <Product key={item.id} product={item} addToCart={props.addToCart}/>
+        return <Product key={item.id} product={item} addToCart={addToCart}/>
       })} 
        </div>
        
